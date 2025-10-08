50f125b81316bf6d1789de187db7fe70f8ef81cd
Dennis Lindqvist <dennis.lindqvist68@outlook.com>
2025-10-08 17:12:57 +0200

    feat: Added JScript for calling PowerShell Pipeline

---
+++ Scripts/Call_GitWrapPipe.js
@@ -0,0 +1,21 @@
+// cmd.exe /c echo show > \\.\pipe\GitWrap
+
+var args = WScript.Arguments;
+
+if (args.length === 0) {
+    WScript.Echo("No arguments provided.");
+    WScript.Quit(-1);
+}
+
+// Command to send
+var command = args(0);
+
+var shell = new ActiveXObject("WScript.Shell");
+
+// Build the cmd.exe command
+// /c = execute and exit
+// > \\.\pipe\GitWrap sends text to the pipe
+var cmdCommand = 'cmd.exe /c echo ' + command + ' > \\\\.\\pipe\\GitWrap';
+
+// Run hidden (0) and wait until finished (true)
+shell.Run(cmdCommand, 0, true);
